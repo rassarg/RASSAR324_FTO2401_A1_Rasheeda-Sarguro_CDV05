0982d35959be13b82efa7144caeb9b06d007430e
rasheeda <raskieda@gmail.com>
2024-02-13 15:45:52 +0200

    added images and personalised

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@ const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
+    document.body.classList.toggle('nav-open'); /* allows us to open the hamburger when clicked*/
 });
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
-        document.body.classList.remove('nav-open');
+        document.body.classList.remove('nav-open'); /* closes the hamburger when link is clicked*/
     })
 })
